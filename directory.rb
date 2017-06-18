2bb3ac2c17f7c87e7e4085366981d9b72918a0c2
Gurjinder Massan <gurjinder.massan@gmail.com>
2017-06-18 18:07:46 +0100

    Code altered to populate array from user input

--- directory.rb
+++ directory.rb
@@ -1,35 +1,53 @@
 # to add more information about each student we are storing the data in hashes
-students = [
- {name:"Dr. Hannibal Lecter", cohort: :november},
- {name:"Darth Vader", cohort: :november},
- {name:"Nurse Ratched", cohort: :november},
- {name:"Michael Corleone", cohort: :november},
- {name:"Alex DeLarge", cohort: :november},
- {name:"The Wicked Witch of the West", cohort: :november},
- {name:"Terminator", cohort: :november},
- {name:"Freddy Krueger", cohort: :november},
- {name:"The Joker", cohort: :november},
- {name:"Joffrey Baratheon", cohort: :november},
- {name:"Norman Bates", cohort: :november}
-]
+#students = [
+# {name:"Dr. Hannibal Lecter", cohort: :november},
+# {name:"Darth Vader", cohort: :november},
+# {name:"Nurse Ratched", cohort: :november},
+# {name:"Michael Corleone", cohort: :november},
+# {name:"Alex DeLarge", cohort: :november},
+# {name:"The Wicked Witch of the West", cohort: :november},
+# {name:"Terminator", cohort: :november},
+# {name:"Freddy Krueger", cohort: :november},
+# {name:"The Joker", cohort: :november},
+# {name:"Joffrey Baratheon", cohort: :november},
+# {name:"Norman Bates", cohort: :november}
+#]
 
 def print_header
-	puts "The students of Villains Academy"
-	puts "-------------"
+    puts "The students of Villains Academy"
+    puts "-------------"
 end
 
 def print(students)
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
-	end
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
 end
 
 def print_footer(students)
-	puts "Overall, we have #{students.count} great students."
+    puts "Overall, we have #{students.count} great students."
 end
 
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+        #create empty array
+    students = []
+        #get the first name
+    name = gets.chomp
+        #while the name is not empty, repeat this code
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+       #get another name from the user
+        name = gets.chomp 
+    end
+    #return the array of students
+    students
+end
 #now we write the main body of the program, which calls the methods
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
